9c7900c80e17d00c23336cfc911f1895e3f951ca
JDGarciag12 <jdgarciag12uniandes.edu.co>
2026-04-13 07:46:54 -0500

    PA releales

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Serie } from "./serie.js";
+import { series } from "./data.js";
+ 
+let seriesTbody: HTMLElement = document.getElementById("series-tbody")!;
+renderSeriesInTable(series);
+function renderSeriesInTable(series: Serie[]): void
+{
+    series.forEach(serie => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td><b>${serie.id}</b></td>
+                               <td>${serie.name}</td>
+                               <td>${serie.channel}</td>
+                               <td>${serie.seasons}</td>`;
+        seriesTbody.appendChild(trElement);
+    }); 
+}
+
+function calcularPromedioSeasons(series: Serie[]): number
+{
+    let totalSeasons = 0;
+    series.forEach(serie => totalSeasons += serie.seasons);
+    return totalSeasons / series.length;
+}
+
+function renderPromedioSeasons(series: Serie[]): void
+{
+    const promedio = calcularPromedioSeasons(series);
+    const promedioElement = document.getElementById("promedio-seasons")!;
+    if (promedioElement) {
+        promedioElement.textContent = `Promedio de temporadas: ${promedio.toFixed(2)}`;
+    }
+}
+renderPromedioSeasons(series);
---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+import { series } from "./data.js";
+let seriesTbody = document.getElementById("series-tbody");
+renderSeriesInTable(series);
+function renderSeriesInTable(series) {
+    series.forEach(serie => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td><b>${serie.id}</b></td>
+                               <td>${serie.name}</td>
+                               <td>${serie.channel}</td>
+                               <td>${serie.seasons}</td>`;
+        seriesTbody.appendChild(trElement);
+    });
+}
+function calcularPromedioSeasons(series) {
+    let totalSeasons = 0;
+    series.forEach(serie => totalSeasons += serie.seasons);
+    return totalSeasons / series.length;
+}
+function renderPromedioSeasons(series) {
+    const promedio = calcularPromedioSeasons(series);
+    const promedioElement = document.getElementById("promedio-seasons");
+    if (promedioElement) {
+        promedioElement.textContent = `Promedio de temporadas: ${promedio.toFixed(2)}`;
+    }
+}
+renderPromedioSeasons(series);
